8246bd1c9651ed58b10c33a4ac962c65b0a63d15
David-Lor <17401854+david-lor@users.noreply.github.com>
2019-12-24 17:17:52 +0100

    Sort user saved stops by stop custom or real name

--- vigobusbot/vigobus_api/__init__.py
+++ vigobusbot/vigobus_api/__init__.py
@@ -2,5 +2,5 @@
 Functions to fetch Stop and real-time Bus data from the VigoBusAPI
 """
 
-from .stop_getter import get_stop
-from .bus_getter import get_buses
+from .stop_getter import *
+from .bus_getter import *
--- vigobusbot/vigobus_api/stop_getter.py
+++ vigobusbot/vigobus_api/stop_getter.py
@@ -4,15 +4,17 @@ Get Stop information using the API
 
 # # Native # #
 import json
+import asyncio
+from typing import Union
 
 # # Package # #
 from .requester import http_get
 from .exceptions import manage_exceptions
 
 # # Project # #
-from ..entities import Stop
+from ..entities import *
 
-__all__ = ("get_stop",)
+__all__ = ("get_stop", "get_multiple_stops")
 
 
 async def get_stop(stop_id: int) -> Stop:
@@ -21,3 +23,14 @@ async def get_stop(stop_id: int) -> Stop:
         data = json.loads(result.text)
         stop = Stop(**data)
         return stop
+
+
+async def get_multiple_stops(*stops_ids: int, return_dict: bool = False) -> Union[Stops, StopsDict]:
+    result: Stops = await asyncio.gather(
+        *[get_stop(stop_id) for stop_id in stops_ids]
+    )
+
+    if return_dict:
+        return {stop.stop_id: stop for stop in result}
+    else:
+        return result
