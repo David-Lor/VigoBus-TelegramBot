0f471bf217813e439e6414c7c0bf89ac48ad6a7a
David-Lor <17401854+david-lor@users.noreply.github.com>
2021-09-10 22:10:28 +0200

    Adapt & improvements in HTTP service
    Adapt to upgraded httpx version.
    Replace HTTPError except block with HTTPStatusError, for dealing only with bad status code.
    Calling raise_for_status can be avoided with function argument.

--- vigobusbot/vigobus_api/requester.py
+++ vigobusbot/vigobus_api/requester.py
@@ -6,7 +6,7 @@ Helpers to request HTTP sources
 import urllib.parse
 
 # # Project # #
-from vigobusbot.services.http import http_request, Methods, AsyncResponse
+from vigobusbot.services.http import http_request, Methods, Response
 from vigobusbot.settings_handler import api_settings as settings
 
 __all__ = ("http_get",)
@@ -14,6 +14,6 @@ __all__ = ("http_get",)
 
 async def http_get(
         endpoint, query_params=None, timeout=settings.timeout, retries=settings.retries
-) -> AsyncResponse:
+) -> Response:
     url = urllib.parse.urljoin(settings.url, endpoint)
     return await http_request(method=Methods.GET, url=url, query_params=query_params, timeout=timeout, retries=retries)
