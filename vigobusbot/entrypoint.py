b9b7d84964e27ffcc13bf5f65cef9cf7b2850753
David-Lor <17401854+David-Lor@users.noreply.github.com>
2024-04-20 16:00:04 +0200

    feat: log elapsed time on initialize/teardown

--- vigobusbot/entrypoint.py
+++ vigobusbot/entrypoint.py
@@ -5,6 +5,8 @@ Start Telegram bot using Polling/Webhook methods depending on the settings
 import asyncio
 import atexit
 
+from contexttimer import Timer
+
 from vigobusbot.telegram_bot import Bot, start_polling
 from vigobusbot.services.couchdb import CouchDB
 from vigobusbot.static_handler import load_static_files
@@ -13,21 +15,27 @@ from vigobusbot.logger import logger
 
 
 async def initialize():
-    bot = Bot().set_current_as_singleton()
-    couchdb = CouchDB().set_current_as_singleton()
-    await asyncio.gather(
-        bot.set_commands(),
-        bot.start_background_services(),
-        couchdb.initialize(),
-    )
+    logger.debug("Initializing the app...")
+    with Timer() as timer:
+        bot = Bot().set_current_as_singleton()
+        couchdb = CouchDB().set_current_as_singleton()
+
+        await asyncio.gather(
+            bot.set_commands(),
+            bot.start_background_services(),
+            couchdb.initialize(),
+        )
+
+    logger.bind(elapsed=timer.elapsed).info("Initialization completed")
 
 
 async def teardown():
     logger.debug("Closing the app...")
-    if couchdb := CouchDB.get_instance():
-        await couchdb.close()
+    with Timer() as timer:
+        if couchdb := CouchDB.get_instance():
+            await couchdb.close()
 
-    logger.info("Teardown completed")
+    logger.bind(elapsed=timer.elapsed).info("Teardown completed")
 
 
 def run():
