8648d0850a444745b58334ee37f30fc411569ec6
David-Lor <17401854+David-Lor@users.noreply.github.com>
2024-04-22 22:13:29 +0200

    refactor: main App class

--- vigobusbot/entrypoint.py
+++ vigobusbot/entrypoint.py
@@ -11,50 +11,53 @@ from vigobusbot.telegram_bot.services.stop_messages_deprecation_reminder import 
 from vigobusbot.services.couchdb import CouchDB
 from vigobusbot.static_handler import load_static_files
 from vigobusbot.settings_handler import telegram_settings as settings
-from vigobusbot.utils import SetupTeardown, SingletonHold
+from vigobusbot.utils import SetupTeardown
 from vigobusbot.logger import logger
 
 
-services: SingletonHold[list[SetupTeardown]] = SingletonHold()
+class App(SetupTeardown):
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+        self.services: list[SetupTeardown] = list()
 
+    async def setup(self):
+        logger.debug("Initializing the app...")
+        with Timer() as timer:
+            self.services = [
+                Bot.get_instance(initialize=True),
+                StopMessagesDeprecationReminder.get_instance(initialize=True),
+                CouchDB.get_instance(initialize=True),
+            ]
 
-async def async_setup():
-    logger.debug("Initializing the app...")
-    with Timer() as timer:
-        services.set_value([
-            Bot.get_instance(initialize=True),
-            StopMessagesDeprecationReminder.get_instance(initialize=True),
-            CouchDB.get_instance(initialize=True),
-        ])
+            load_static_files()
+            await asyncio.gather(*[service.setup() for service in self.services])
 
-        await asyncio.gather(*[service.setup() for service in services.get_value()])
+        logger.bind(elapsed=timer.elapsed).info("Initialization completed")
 
-    logger.bind(elapsed=timer.elapsed).info("Initialization completed")
+    async def teardown(self):
+        logger.info("Closing the app...")
+        with Timer() as timer:
+            await asyncio.gather(*[service.teardown() for service in self.services])
 
+        logger.bind(elapsed=timer.elapsed).info("Teardown completed")
 
-async def async_teardown():
-    logger.info("Closing the app...")
-    with Timer() as timer:
-        await asyncio.gather(*[service.teardown() for service in services.get_value()])
+    def run(self):
+        self.loop.run_until_complete(self.setup())
 
-    logger.bind(elapsed=timer.elapsed).info("Teardown completed")
+        try:
+            if settings.webhook_enabled:
+                run_bot_webhook()
+            else:
+                run_bot_polling()
+        except (SystemExit, KeyboardInterrupt):
+            # TODO Exit with corresponding exit code
+            pass
+
+        self.loop.run_until_complete(self.teardown())
 
 
 def run():
-    load_static_files()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_setup())
-
-    try:
-        if settings.webhook_enabled:
-            run_bot_webhook()
-        else:
-            run_bot_polling()
-    except (SystemExit, KeyboardInterrupt):
-        # TODO Exit with corresponding exit code
-        pass
-
-    loop.run_until_complete(async_teardown())
+    App().run()
 
 
 if __name__ == '__main__':
